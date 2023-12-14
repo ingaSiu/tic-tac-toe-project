f1389d84d6a192e7a7d4ae70a1a8080109c3b70b
ingaSiu <ingasiud@gmail.com>
2023-12-14 18:52:16 +0200

    map board component

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,7 +1,23 @@
 import './Board.css';
 
+import Square from '../Square/Square';
+
 const Board = () => {
-  return <div>Board</div>;
+  const boardSize = 3;
+  const boardRows = Array.from(Array(boardSize).keys());
+
+  return (
+    <div>
+      {boardRows.map((row, rowIndex) => (
+        <div key={rowIndex} className="boardRow">
+          {boardRows.map((col, colIndex) => {
+            const squareIndex = rowIndex * boardSize + colIndex;
+            return <Square key={squareIndex} />;
+          })}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Board;
