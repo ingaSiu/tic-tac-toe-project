417215a0f8f3ed63c6e33a17c3a576b8a26caa46
ingaSiu <ingasiud@gmail.com>
2023-12-14 19:02:55 +0200

    position in the middle

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import Board from './components/Board/Board';
 
 const App = () => {
   return (
-    <>
+    <div className="mainWrapper">
       <h1>Hello there</h1>
       <Board />
-    </>
+    </div>
   );
 };
 
