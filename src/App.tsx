956d6b6c1f0ada442cb6dd50c23fe581aeb2003c
ingaSiu <ingasiud@gmail.com>
2023-12-14 18:51:47 +0200

    import board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css';
 
+import Board from './components/Board/Board';
+
 const App = () => {
-  return <h1>Hello there</h1>;
+  return (
+    <>
+      <h1>Hello there</h1>
+      <Board />
+    </>
+  );
 };
 
 export default App;
