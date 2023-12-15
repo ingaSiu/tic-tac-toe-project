131b0a6fefd469fdb67f01c29efa30f4da8337cd
ingaSiu <ingasiud@gmail.com>
2023-12-15 18:14:26 +0200

    add onClick functionality

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,21 +2,44 @@ import './Board.css';
 
 import Square from '../Square/Square';
 
-const Board = () => {
+type BoardProps = {
+  xIsNext: boolean;
+  squares: string[];
+  onPlay: () => void;
+};
+
+const Board = ({ xIsNext, squares, onPlay }: BoardProps) => {
+  const handleClick = (i: number) => {
+    if (squares[i]) {
+      return;
+    }
+
+    const nextSquares = squares.slice();
+
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+
+    onPlay(nextSquares);
+  };
+  const isBoardFull = squares.every((square) => square !== null);
+
   const boardSize = 3;
   const boardRows = Array.from(Array(boardSize).keys());
 
   return (
-    <div>
+    <>
       {boardRows.map((row, rowIndex) => (
         <div key={rowIndex} className="boardRow">
           {boardRows.map((col, colIndex) => {
             const squareIndex = rowIndex * boardSize + colIndex;
-            return <Square key={squareIndex} />;
+            return <Square key={squareIndex} value={squares[squareIndex]} onSquareClick={handleClick(squareIndex)} />;
           })}
         </div>
       ))}
-    </div>
+    </>
   );
 };
 
