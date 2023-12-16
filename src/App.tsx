86aa9531e051aad508afb80850965ed2e68605e1
ingaSiu <ingasiud@gmail.com>
2023-12-16 17:35:37 +0200

    handle play

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css';
 
 import Board from './components/Board/Board';
+import { useState } from 'react';
 
 const App = () => {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handlePlay = (nextSquares: string[]) => {
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  };
   return (
     <div className="mainWrapper">
       <h1>Hello there</h1>
-      <Board />
+      <Board xIsNext={xIsNext} squares={squares} onPlay={handlePlay} />
     </div>
   );
 };
