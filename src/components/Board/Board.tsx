742f4e110e7574159170e5c22d77f75e75ad0a6d
ingaSiu <ingasiud@gmail.com>
2023-12-16 17:35:23 +0200

    add turn, game end logic

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,11 +1,12 @@
 import './Board.css';
 
 import Square from '../Square/Square';
+import { calculateWinner } from '../../utils/calculateWinner';
 
 type BoardProps = {
   xIsNext: boolean;
   squares: string[];
-  onPlay: () => void;
+  onPlay: (nextSquares: string[]) => void;
 };
 
 const Board = ({ xIsNext, squares, onPlay }: BoardProps) => {
@@ -26,16 +27,32 @@ const Board = ({ xIsNext, squares, onPlay }: BoardProps) => {
   };
   const isBoardFull = squares.every((square) => square !== null);
 
+  const winnerInfo = calculateWinner(squares);
+  const winningLine = winnerInfo ? winnerInfo.winningLine : null;
+
+  let status;
+  if (winnerInfo) {
+    status = `Winner: ${winnerInfo.winner}!`;
+  } else if (isBoardFull) {
+    status = 'Draw! The game is a tie!';
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
   const boardSize = 3;
   const boardRows = Array.from(Array(boardSize).keys());
 
   return (
     <>
+      <div className="status">{status}</div>
       {boardRows.map((row, rowIndex) => (
         <div key={rowIndex} className="boardRow">
           {boardRows.map((col, colIndex) => {
             const squareIndex = rowIndex * boardSize + colIndex;
-            return <Square key={squareIndex} value={squares[squareIndex]} onSquareClick={handleClick(squareIndex)} />;
+
+            return (
+              <Square key={squareIndex} value={squares[squareIndex]} onSquareClick={() => handleClick(squareIndex)} />
+            );
           })}
         </div>
       ))}
