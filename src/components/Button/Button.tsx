01b6ed951f57488faa3cb508f82ee92d3528cab1
ingaSiu <ingasiud@gmail.com>
2023-12-17 17:00:28 +0200

    create button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import './Button.css';
+
+type ButtonProps = {
+  children: React.ReactNode;
+};
+
+const Button = ({ children }: ButtonProps) => {
+  return <button>{children}</button>;
+};
+
+export default Button;
