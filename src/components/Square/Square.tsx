77e8a861b33418fab07bfc2d217f207a3e4392e3
ingaSiu <ingasiud@gmail.com>
2023-12-17 17:00:17 +0200

    add prop, conditional statement for styling

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -3,11 +3,12 @@ import './Square.css';
 type SquareProps = {
   value: string | null;
   onSquareClick: () => void;
+  isWinning: boolean | null;
 };
 
-const Square = ({ value, onSquareClick }: SquareProps) => {
+const Square = ({ value, onSquareClick, isWinning }: SquareProps) => {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className={`square ${isWinning ? 'winning' : ''}`} onClick={onSquareClick}>
       {value}
     </button>
   );
